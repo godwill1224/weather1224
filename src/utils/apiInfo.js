7b53fef1432d5a6ec1586bb3852ebefe4e850802
Mawa Godwill <97454201+godwill1224@users.noreply.github.com>
2023-06-23 12:02:47 +0300

    added api key

--- src/utils/apiInfo.js
+++ src/utils/apiInfo.js
@@ -1,3 +1,3 @@
-export const apiKey = process.env.REACT_APP_ACCESS_KEY;
+export const apiKey = "72ef42c7655db817b0ca11321457be04";
 
 export const BASE_URL = "https://api.openweathermap.org/data/2.5";
